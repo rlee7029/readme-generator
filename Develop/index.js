d5ae26036df723ffc7cbd2e3edf74ed27128be38
Robert Lee <robertlee@Roberts-MBP.attlocal.net>
2023-04-27 01:36:22 -0700

    added user input, fuction fo readme, and init

--- Develop/index.js
+++ Develop/index.js
@@ -4,5 +4,80 @@ const fs = require('fs')
 const util = require('util')
 
 const generateMarkdown = require('./utils/generateMarkdown');
-const { default: inquirer } = require("inquirer");
 // TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'what is the description of your project?:'
+
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Installation Process?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How is your software used?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: "What kind of license does your project have? Please choose from the following...",
+        choices: ['MIT', 'Apache', 'GPL']
+    },
+    {
+        type: 'input',
+        name: 'contributions',
+        message: 'What is the contricbution of this project?'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Test of your project?'
+    },
+    {
+        type:'input',
+        name:'question',
+        message:'Do you have any questions?'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub user name?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+
+];
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) { 
+    fs.writeFile(fileName, data, error => {
+    if (error) {
+      return console.log(error);
+    }
+  
+    console.log("Success! Your README.md file has been generated")
+});
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        writeToFile(answers)
+
+    })
+
+}
