938194b440946a3830a3d4685bad31cad90202fb
Robert Lee <robertlee@Roberts-MBP.attlocal.net>
2023-04-27 01:08:45 -0700

    added indes.js package for the app

---
+++ Develop/index.js
@@ -0,0 +1,8 @@
+// TODO: Include packages needed for this application
+const inquier = require("inquirer")
+const fs = require('fs')
+const util = require('util')
+
+const generateMarkdown = require('./utils/generateMarkdown');
+const { default: inquirer } = require("inquirer");
+// TODO: Create an array of questions for user input
